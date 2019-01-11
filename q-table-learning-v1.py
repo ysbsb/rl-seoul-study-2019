6ec906c906fc7fe925572589c409e5f832325cce
subin-yang <ysn8049@naver.com>
2019-01-11 16:00:05 +0900

    Learn path in Frozenlake Game using Q-table learning algorithm
    Agent goes path in every episodes. Q-table learning algorithm is
    used for learning path.

---
+++ q-table-learning-v1.py
@@ -0,0 +1,52 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+from gym.envs.registration import register
+
+# Register FrozenLake with is_slippery False
+register(
+    id='FrozenLake-v3',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name' : '4x4', 'is_slippery' : False}
+)
+
+env = gym.make('FrozenLake-v3')
+env.render()  # Show the initial board
+
+# Initialize table with all zeros
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+# Discount factor
+dis = .99
+# Set learning parameters
+num_episodes = 2000
+
+# create lists to contain totoal rewards and steps per episode
+rList = []
+for i in range(num_episodes):
+    # Reset environment and get first new obervation
+    state = env.reset()
+    rAll = 0
+    done = False
+
+    # The Q-Table learning algorithm
+    while not done:
+        # Chooese an action by greedily (with noise) piking form Q table
+        action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) / (0.01*(i+1)))
+
+        # Get new state and reward from environment
+        new_state, reward, done, _ = env.step(action)
+
+        # Update Q-Table with new knowledge using decay rate
+        Q[state, action] = reward + dis * np.max(Q[new_state, :])
+
+        rAll += reward
+        state = new_state
+
+    rList.append(rAll)
+
+print("Success rate: " + str(sum(rList)/num_episodes))
+print("Final Q-Table Values")
+print("LEFT DOWN RIGHT UP")
+print(Q)
+plt.bar(range(len(rList)), rList, color="blue")
+plt.show()
